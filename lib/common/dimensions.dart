95c41116961462e26770b554311ecb349d62556d
flutterry <ahmed01289017198@gmail.com>
2021-06-22 16:24:26 +0200

    solve dimensions problem

--- lib/common/dimensions.dart
+++ lib/common/dimensions.dart
@@ -5,7 +5,7 @@ double get_height(int percent) => Get.height * get_percent(percent);
 
 double get_percent(int percent) {
   int p = percent < 0 ? 0 : percent;
-  p = percent > 0 ? 100 : percent;
+  p = percent > 100 ? 100 : percent;
 
   return p / 100;
 }
